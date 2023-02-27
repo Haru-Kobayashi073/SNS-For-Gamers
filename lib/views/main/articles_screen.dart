4ec499284c496425cc5dce7da0fa1509b86c17d9
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-02-27 17:55:15 +0900

    SteamAPIの情報をListTileに格納して表示

--- lib/views/main/articles_screen.dart
+++ lib/views/main/articles_screen.dart
@@ -22,25 +22,102 @@ class ArticleScreen extends ConsumerWidget {
     final ThemeModel themeModel = ref.watch(themeProvider);
     final articles = articlesModel.articles;
     final newsLists = articlesModel.newsList;
+    final maxWidth = MediaQuery.of(context).size.width;
+    final maxHeight = MediaQuery.of(context).size.height;
     return Scaffold(
+      backgroundColor: colors.backScreenColor,
       appBar: NormalAppBar(title: articleText, mainModel: mainModel),
-      body: Container(
-        decoration: const BoxDecoration(color: colors.backScreenColor),
-        alignment: Alignment.center,
-        child: ListView.builder(
-            itemCount: newsLists.length,
-            itemBuilder: (context, index) {
-              final newsList = newsLists[index];
-              return ListTile(
-                title: Text(
-                  newsList['title'],
-                  style: const TextStyle(color: colors.listTileTextColor),
-                  overflow: TextOverflow.ellipsis,
-                ),
-                onTap: () {},
-              );
-            }),
-      ),
+      body: ListView.builder(
+          itemCount: newsLists.length,
+          itemBuilder: (context, index) {
+            final newsList = newsLists[index];
+            final url = articlesModel.fetchImgToUrl(index: index);
+            return ListTile(
+              leading: Image.network(
+                  'https://cdn.akamai.steamstatic.com/steamcommunity/public/images/clans/38098458/85af5c722fa902f45b49a2f3a5c0daeb9e4eb976.jpg'),
+              // leading: Container(
+              //   width: 100,
+              //   child: Image.network(Uri.parse(articlesModel.articleImgUrl!).toString())
+              //   ),
+              title: Text(
+                newsList['title'],
+                style: const TextStyle(color: colors.listTileTextColor),
+                overflow: TextOverflow.ellipsis,
+              ),
+              subtitle: Text(
+                // newsList['date'].toString()
+                articlesModel.convertTime(newsList['date']),
+                style: const TextStyle(color: colors.cardTextPrimaryColor),
+                // 'qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm'
+              ),
+              onTap: () async =>
+                  await articlesModel.launchToBrawser(url: newsList['url']),
+            );
+            //     Padding(
+            //   padding: const EdgeInsets.all(16.0),
+            //   child: Material(
+            //     elevation: 20,
+            //     color: const Color.fromARGB(255, 216, 218, 216),
+            //     borderRadius: BorderRadius.circular(30),
+            //     child: Row(
+            //       crossAxisAlignment: CrossAxisAlignment.start,
+            //       children: [
+            //         Container(
+            //           padding: const EdgeInsets.symmetric(
+            //               horizontal: 30, vertical: 16),
+            //           child: Column(
+            //             children: [
+            //               SizedBox(
+            //                 height: maxHeight * 0.5,
+            //                 child: Text(
+            //                   newsList['title'],
+            //                   maxLines: 20,
+            //                   style: const TextStyle(
+            //                       color: colors.cardTextPrimaryColor,
+            //                       fontWeight: FontWeight.bold),
+            //                   overflow: TextOverflow.ellipsis,
+            //                 ),
+            //               ),
+            //               const Text(
+            //                 'aaaaaaaa',
+            //                 style: TextStyle(
+            //                     color: colors.cardTextPrimaryColor),
+            //                 )
+            //             ],
+            //           ),
+            //         ),
+            //         Padding(
+            //           padding: const EdgeInsets.all(16.0),
+            //           child: Container(
+            //             width: maxWidth * 0.4,
+            //             child: Image.network(
+            //                 "https://cdn.akamai.steamstatic.com/steamcommunity/public/images/clans/38098458/85af5c722fa902f45b49a2f3a5c0daeb9e4eb976.jpg"),
+            //           ),
+            //         )
+            //       ],
+            //     ),
+            //   ),
+            // );
+            //     Text(
+            //   newsList['title'],
+            //   style: const TextStyle(color: colors.listTileTextColor),
+            //   // overflow: TextOverflow.ellipsis,
+            //   maxLines: 20,
+            // );
+            //     Container(
+            //       width: maxWidth * 0.1,
+            //   child: Row(
+            //     children: [
+            //       Text(
+            //         newsList['title'],
+            //         maxLines: 1,
+            //         style: const TextStyle(color: colors.listTileTextColor),
+            //         overflow: TextOverflow.ellipsis,
+            //       ),
+            //     ],
+            //   ),
+            // );
+          }),
     );
   }
 }
