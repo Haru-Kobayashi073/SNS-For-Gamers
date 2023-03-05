01917f52a8168a1241ff2da5d8b424aa0d5b413d
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-03-05 20:06:08 +0900

    videoWidgetのロードWidgetの表示範囲を制限

--- lib/views/main/components/video_watch_page.dart
+++ lib/views/main/components/video_watch_page.dart
@@ -1,19 +1,12 @@
-import 'package:chewie/chewie.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:video_player/video_player.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:chewie/chewie.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoWatchPage extends ConsumerStatefulWidget {
   //VideoItemを実行した時の引数を取得するのに必要。
-  // final dynamic saveVideo;
   final String videoPath;
-  // final bool isPreviewPage;
-  const VideoWatchPage(
-    // this.saveVideo,
-    this.videoPath,
-      // this.isPreviewPage,
-      {super.key});
-
+  const VideoWatchPage(this.videoPath, {super.key});
   @override
   _VideoWatchPageState createState() => _VideoWatchPageState();
 }
@@ -26,7 +19,6 @@ class _VideoWatchPageState extends ConsumerState<VideoWatchPage> {
   late Future<void> _future;
 
   Future<void> initVideoPlayer() async {
-    //初期化。
     await _videoPlayerController.initialize();
     setState(() {
       //Chewieで動画を再生するための準備。
@@ -36,9 +28,9 @@ class _VideoWatchPageState extends ConsumerState<VideoWatchPage> {
         //動画表示画面の比率
         aspectRatio: _videoPlayerController.value.aspectRatio,
         //自動再生
-        autoPlay: true,
+        autoPlay: false,
         //自動ループ再生
-        looping: true,
+        looping: false,
         //もし、Chewieでエラーが出ていたら表示。
         //The requested URL was not found on this server.と出てきたら動画のパスの型（File型、String型）に注意。
         errorBuilder: (context, errorMessage) {
@@ -49,18 +41,13 @@ class _VideoWatchPageState extends ConsumerState<VideoWatchPage> {
         },
       );
     });
-    print(_videoPlayerController.value.aspectRatio);
   }
 
   @override
   void initState() {
     super.initState();
     //_videoPlayerControllerに動画のパスを示す。widget.’変数’で引数を用いれる。
-    // _videoPlayerController = VideoPlayerController.file(widget.saveVideo);
     _videoPlayerController = VideoPlayerController.network(widget.videoPath);
-
-    // _videoPlayerController = VideoPlayerController.network(
-    //     'https://firebasestorage.googleapis.com/v0/b/sns-volume2.appspot.com/o/users%2F3xaiU7NZGRTgSpbnYyLKGhEQmQM2%2Fabe0cfa0-56d1-4a15-be33-40c0c60bc281.mp4?alt=media&token=095683d5-809d-4e44-a5c5-f2f4f044aa42');
     _future = initVideoPlayer();
   }
 
@@ -74,61 +61,34 @@ class _VideoWatchPageState extends ConsumerState<VideoWatchPage> {
   }
 
   @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       title: const Text("video"),
-  //     ),
-  //     body: SafeArea(
-  //       child: FutureBuilder(
-  //         //_future = initVideoPlayer()
-  //         future: _future,
-  //         builder: (context, snapshot) {
-  //           return Center(
-  //             //initializedされていたらtrueになって、動画が再生される。
-  //             child: _videoPlayerController.value.isInitialized
-  //                 //動画再生表示画面の比率をオリジナルのまま表示してくれる
-  //                 ? AspectRatio(
-  //                     aspectRatio: _videoPlayerController.value.aspectRatio,
-  //                     //数行で動画再生してくれる。
-  //                     child: Chewie(
-  //                       controller: _chewieController,
-  //                     ),
-  //                   )
-  //                 : const CircularProgressIndicator(),
-  //           );
-  //         },
-  //       ),
-  //     ),
-  //   );
-  // }
-
   Widget build(BuildContext context) {
     final maxWidth = MediaQuery.of(context).size.width;
-    final maxHeight = MediaQuery.of(context).size.height;
 
     return SafeArea(
       child: FutureBuilder(
         //_future = initVideoPlayer()
         future: _future,
         builder: (context, snapshot) {
-          return Container(
-            // width: 124 * _videoPlayerController.value.aspectRatio,
-            width: maxWidth * 0.68,
-            // height: 124,
-            //initializedされていたらtrueになって、動画が再生される。
-            child: _videoPlayerController.value.isInitialized
-                //動画再生表示画面の比率をオリジナルのまま表示してくれる
-                ? AspectRatio(
+          return _videoPlayerController.value.isInitialized
+              ? SizedBox(
+                  width: maxWidth * 0.68,
+                  //initializedされていたらtrueになって、動画が再生される。
+                  child:
+                      //動画再生表示画面の比率をオリジナルのまま表示してくれる
+                      AspectRatio(
                     aspectRatio: _videoPlayerController.value.aspectRatio,
                     //数行で動画再生してくれる。
                     child: Chewie(
                       controller: _chewieController,
                     ),
-                  )
-                // : const SizedBox(child: CircularProgressIndicator()),
-                : const Text('wait a minute')
-          );
+                  ),
+                )
+              : Container(
+                  alignment: Alignment.center,
+                  width: maxWidth * 0.68,
+                  child: const SizedBox(
+                      width: 40, child: CircularProgressIndicator()),
+                );
         },
       ),
     );
