c9590a8dad5567d31c483fab4cf467432e87ab97
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-03-07 23:56:39 +0900

    drawerのリファクタリング

--- lib/details/sns_drawer.dart
+++ lib/details/sns_drawer.dart
@@ -5,9 +5,9 @@ import 'package:sns_vol2/constants/strings.dart';
 import 'package:sns_vol2/constants/colors.dart' as colors;
 //models
 import 'package:sns_vol2/models/main_model.dart';
+
 class SNSDrawer extends StatelessWidget {
-  const SNSDrawer({Key? key, required this.mainModel})
-      : super(key: key);
+  const SNSDrawer({Key? key, required this.mainModel}) : super(key: key);
   final MainModel mainModel;
 
   @override
@@ -17,33 +17,48 @@ class SNSDrawer extends StatelessWidget {
       child: ListView(
         children: [
           ListTile(
-            title: const Text(accountTitle, style: TextStyle(color: colors.listTileTextColor),),
+            title: const Text(
+              accountTitle,
+              style: TextStyle(color: colors.white),
+            ),
             onTap: () =>
                 routes.toAccountPage(context: context, mainModel: mainModel),
           ),
           ListTile(
-            title: const Text(muteUsersPageTitle, style: TextStyle(color: colors.listTileTextColor),),
+            title: const Text(
+              muteUsersPageTitle,
+              style: TextStyle(color: colors.white),
+            ),
             onTap: () =>
                 routes.toMuteUsersPage(context: context, mainModel: mainModel),
           ),
           ListTile(
-            title: const Text(mutePostsPageTitle, style: TextStyle(color: colors.listTileTextColor),),
-            onTap: () =>
-                routes.toMutePostsPage(context: context, mainModel: mainModel)
-          ),
+              title: const Text(
+                mutePostsPageTitle,
+                style: TextStyle(color: colors.white),
+              ),
+              onTap: () => routes.toMutePostsPage(
+                  context: context, mainModel: mainModel)),
           ListTile(
-            title: const Text(muteCommentsPageTitle, style: TextStyle(color: colors.listTileTextColor),),
-            onTap: () =>
-                routes.toMuteCommentsPage(context: context, mainModel: mainModel)
-          ),
+              title: const Text(
+                muteCommentsPageTitle,
+                style: TextStyle(color: colors.white),
+              ),
+              onTap: () => routes.toMuteCommentsPage(
+                  context: context, mainModel: mainModel)),
           ListTile(
-            title: const Text(muteRepliesPageTitle, style: TextStyle(color: colors.listTileTextColor),),
-            onTap: () =>
-                routes.toMuteRepliesPage(context: context, mainModel: mainModel)
-          ),
+              title: const Text(
+                muteRepliesPageTitle,
+                style: TextStyle(color: colors.white),
+              ),
+              onTap: () => routes.toMuteRepliesPage(
+                  context: context, mainModel: mainModel)),
           if (mainModel.firestoreUser.isAdmin)
             ListTile(
-              title: const Text(adminTitle, style: TextStyle(color: colors.listTileTextColor),),
+              title: const Text(
+                adminTitle,
+                style: TextStyle(color: colors.white),
+              ),
               onTap: () =>
                   routes.toAdminPage(context: context, mainModel: mainModel),
             )
