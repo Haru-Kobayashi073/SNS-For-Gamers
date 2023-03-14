a4865e3c0e0f78281dfe3509e362b2f2c0b6edab
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-03-15 00:58:14 +0900

    NormalAppBarクラスのリファクタリング

--- lib/details/normal_appbar.dart
+++ lib/details/normal_appbar.dart
@@ -1,21 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:sns_vol2/constants/colors.dart' as colors;
-import 'package:sns_vol2/details/drawer_container.dart';
-import 'package:sns_vol2/details/sns_drawer.dart';
-import 'package:sns_vol2/models/main/normal_appbar_model.dart';
-import 'package:sns_vol2/models/main_model.dart';
-import 'package:sns_vol2/models/themes_model.dart';
 
 class NormalAppBar extends StatelessWidget with PreferredSizeWidget {
   const NormalAppBar({
     super.key,
-    required this.title,
-    // required this.boolValue,
-    required this.mainModel,
+    required this.title
   });
   final String title;
-  // final bool boolValue;
-  final MainModel mainModel;
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +15,7 @@ class NormalAppBar extends StatelessWidget with PreferredSizeWidget {
       backgroundColor: colors.green,
       title: Text(
         title,
-        style: const TextStyle(color: colors.appBarTextColor),
+        style: const TextStyle(color: colors.white),
       ),
     );
   }
@@ -32,62 +23,3 @@ class NormalAppBar extends StatelessWidget with PreferredSizeWidget {
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
-
-// class NormalAppBar extends StatefulWidget implements PreferredSizeWidget {
-//   const NormalAppBar({super.key, required this.title, required this.boolValue
-//       // required this.normalAppBarModel
-//       });
-//   final String title;
-//   final bool boolValue;
-
-//   @override
-//   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
-//   @override
-//   State<NormalAppBar> createState() => _NormalAppBarState();
-// }
-
-// class _NormalAppBarState extends State<NormalAppBar> {
-//   void showDrawers() {
-//     print('tapped on show drawer!');
-//     setState(() {
-//       showDrawer = !showDrawer;
-//     });
-//   }
-
-//   bool showDrawer = false;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final bool drawersBoolSet = widget.boolValue;
-//     return Stack(children: [
-//       Scaffold(
-//         // drawer: showDrawer == true ? Drawer(child: SNSDrawer(mainModel: mainModel, themeModel: themeModel),),
-//         appBar: PreferredSize(
-//           preferredSize: const Size.fromHeight(100),
-//           child: AppBar(
-//               iconTheme: const IconThemeData(color: colors.appBarTextColor),
-//               elevation: 0,
-//               backgroundColor: colors.appBarBackColor,
-//               title: Text(
-//                 widget.title,
-//                 style: const TextStyle(color: colors.appBarTextColor),
-//               ),
-//               // leading: drawersBoolSet == true
-//               //     ? IconButton(
-//               //         onPressed: () => showDrawers(),
-//               //         icon: const Icon(Icons.notes_rounded),
-//               //         iconSize: 30,
-//               //       )
-//               //     : IconButton(
-//               //         onPressed: () => Navigator.pop(context),
-//               //         icon: const Icon(Icons.arrow_back_ios))
-//                       ),
-//         ),
-//       ),
-//       // if (showDrawer)
-//       //   DrawerWidget(
-//       //     closeFunction: showDrawers,
-//       //   ),
-//     ]);
-//   }
-// }
