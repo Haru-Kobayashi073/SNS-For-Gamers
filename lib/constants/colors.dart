ddb601e938cb1fc1badc355d9d982c778fafeaec
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-03-27 19:28:22 +0900

    カラーを整理

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,36 +1,9 @@
 import 'package:flutter/material.dart';
 
-//ポストカードコンテナ内のテキストテーマ
-const Color cardTextPrimaryColor = Color.fromARGB(255, 59, 107, 80);
+const Color green = Color.fromARGB(255, 59, 107, 80);
+const Color white = Colors.white;
+const Color black = Colors.black;
+Color shadeGrey = Colors.grey.shade200;
+const Color greenishWhite = Color.fromARGB(255, 225, 231, 225);
 const Color cardBackColor = Color.fromARGB(255, 225, 231, 225);
-
-//メインコンテンツ内のテキストテーマ
-const Color mainTextPrimaryColor = Colors.white;
-
-//ボトムナビゲーションのテキストテーマ
-const Color bottomNaviBarTextColor = Color.fromARGB(255, 225, 231, 225);
-const Color bottomNaviUnselectedColor = Color.fromARGB(255, 187, 187, 187);
-const Color bottomNaviBarBackColor = Color.fromARGB(255, 59, 107, 80);
-
-// AppBarのテキストテーマ
-// const Color appBarTextColor = Color.fromARGB(255, 225, 231, 225);
-const Color appBarTextColor = Colors.white;
-
-const Color appBarBackColor = Color.fromARGB(255, 59, 107, 80);
-
-// const Color floatingButtonBackColor = Color(0xffD6A34A);
-const Color floatingButtonBackColor = Colors.black;
-const Color floatingButtonIconColor = Color.fromARGB(255, 225, 231, 225);
-
-const Color backScreenColor = Color.fromARGB(255, 59, 107, 80);
-
-const Color listTileTextColor = Colors.white;
-
-const Color profileTextColor = Colors.white;
-
-const Color postDialogIconColor = Color.fromARGB(255, 59, 107, 80);
-
-const Color profileSomeButtonBackColor = Color.fromARGB(255, 225, 231, 225);
-const Color profileSomeButtonTextColor = Color.fromARGB(255, 59, 107, 80);
-
-const Color searchBarBackColor = Color.fromARGB(255, 225, 231, 225);
+const Color grey = Color.fromARGB(255, 187, 187, 187);
